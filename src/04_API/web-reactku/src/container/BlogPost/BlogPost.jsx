00702fefe43ea286b8ff1ab54dd06b89f078e91c
fitriMD <mutiara.devi027@gmail.com>
2022-03-11 22:13:24 +0700

    Js4 Prak4

--- src/04_API/web-reactku/src/container/BlogPost/BlogPost.jsx
+++ src/04_API/web-reactku/src/container/BlogPost/BlogPost.jsx
@@ -4,11 +4,17 @@ import Post from "../../component/BlogPost/Post";
 
 class BlogPost extends Component {
     state = {               //komponen state dari React untuk statefull component
-        listArtikel: []     //variabel array yang digunakan untuk menyimpan data API
+        listArtikel: [],    //variabel array yang digunakan untuk menyimpan data API
+        insertArtikel: {    //variabel yang digunakan untuk menampung sementara data yang akan di insert
+            userId: 1,      // kolom userId, id, title, dan body sama, mengikuti kolom yang ada pada listArtikel.json
+            id: 1,
+            title: "",
+            body: ""
+        }
     }
 
-    ambilDataDariServerAPI() {
-        fetch('http://localhost:3001/posts') //alamat URL API yang ingin kita ambil datanya
+    ambilDataDariServerAPI = () => {                      //fungsi untuk mengambil data dari API dengan menambahkan sort dan order
+        fetch('http://localhost:3001/posts?_sort=id&_order=desc')        //penambahan sort dan order berdasarkan parameter
             .then(response => response.json())      //ubah response data dari URL API menjadi sebuah data json
             .then(jsonHasilAmbilDariAPI => {        //data json hasil ambil dari API kita masukkan ke dalam listArtikel pada state
                 this.setState({
@@ -28,9 +34,48 @@ class BlogPost extends Component {
             })
     }
 
+    handleTambahArtikel = (event) => {      //fungsi untuk meng-handle form tambah data artikel
+        let formInsertArtikel = { ...this.state.insertArtikel };        //clonning data state insertArtikel ke dalam variabel formInsertArtikel
+        let timestamp = new Date().getTime();                           //digunakan untuk menyimpan waktu (sebagi ID artikel)
+        formInsertArtikel['id'] = timestamp;
+        formInsertArtikel[event.target.name] = event.target.value;      //menyimpan data onchange ke formInsertArtikel sesuai dengan target yg diisi
+        this.setState({
+            insertArtikel: formInsertArtikel
+        });
+    }
+
+    handleTombolSimpan = () => {            //fungsi untuk meng-handle tombol simpan
+        fetch('http://localhost:3001/posts', {
+            method: 'post',                             //method POST untuk input/insert data
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.state.insertArtikel)      //kirimkan ke body request untuk data artikel yang akan ditambahkan (insert)
+        })
+            .then((Response) => {
+                this.ambilDataDariServerAPI();      //reload/refresh data
+            });
+    }
+
     render() {
         return (
             <div className="post-artikel">
+                <div className="form pb-2 border-buttom">
+                    <div className="form-group row">
+                        <label htmlFor="title" className="col-sm-2 col-form-label">Judul</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" id="title" name="title" onChange={this.handleTambahArtikel} />
+                        </div>
+                    </div>
+                    <div className="form-group-row">
+                        <label htmlFor="body" className="col-sm-2 col-form-label">Isi</label>
+                        <div className="col-sm-10">
+                            <textarea className="form-control" id="body" name="body" rows="3" onChange={this.handleTambahArtikel}></textarea>
+                        </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary" onClick={this.handleTombolSimpan}>Simpan</button>
+                </div>
                 <h2>Daftar Artikel</h2>
                 {
                     this.state.listArtikel.map(artikel => { //looping dan masukkan untuk setiap data yang ada di listArtikel ke variabel artikel
