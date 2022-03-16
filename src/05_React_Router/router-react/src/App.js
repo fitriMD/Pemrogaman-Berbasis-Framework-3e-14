4988f4dd4ecee499017a520bc7a887ccfb4ee33c
fitriMD <mutiara.devi027@gmail.com>
2022-03-16 10:24:05 +0700

    prak URL Parameters

--- src/05_React_Router/router-react/src/App.js
+++ src/05_React_Router/router-react/src/App.js
@@ -3,65 +3,48 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  useParams
 }
   from "react-router-dom";
 
 //params adalah placeholder di URL yang dimulai dengan titik dua,
 // seperti param ': id' yang didefinisikan dalam route dalam contoh ini.
 
-export default function BasicExample() {
+export default function ParamsExample() {
   return (
     <Router>
       <div>
+        <h2>Accounts</h2>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/netflix">Netflix</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/gmail">Gmail</Link>
           </li>
           <li>
-            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/yahoo">Yahoo</Link>
+          </li>
+          <li>
+            <Link to="/amazon">Amazon</Link>
           </li>
         </ul>
         <hr />
         <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
+          <Route path="/:id" children={<Child />}/>
         </Switch>
       </div>
     </Router>
   );
 }
 
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
+function Child() {
+  let { id } = useParams();
 
-function About() {
   return (
     <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
+      <h3>ID: {id}</h3>
     </div>
   );
 }
